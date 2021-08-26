818acb4f604fa77ddeb57af8e76e419e9f09859d
Kumari Sarita <44333330+sarita5969@users.noreply.github.com>
2021-08-26 21:41:55 +0530

    Add files via upload

---
+++ Sarita_palindrome.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+func main(){
+	fmt.Println("Enter the number")
+	var num int
+	fmt.Scanln(&num)
+	Ispalindrome(num)
+}
+
+func Ispalindrome(n int){
+	temp := n
+	var newn int = 0
+	for n:=n;n>0;n=n/10 {
+		no := n%10
+		newn += no*no*no
+	}
+	if temp == newn{
+		fmt.Printf(" %d is palindrome number\n", temp)
+	} else {
+		fmt.Printf(" %d is not palindrome number\n\n", temp)
+	}
+}
